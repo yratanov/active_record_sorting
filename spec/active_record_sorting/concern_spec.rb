3cbc569720619beb32f8a5d5f3c349c65e3839da
yratanov <organium@gmail.com>
2015-07-12 20:24:25 +0700

    fix specs

--- spec/active_record_sorting/concern_spec.rb
+++ spec/active_record_sorting/concern_spec.rb
@@ -1,39 +1,40 @@
 require 'spec_helper'
 
 describe ActiveRecordSorting::Concern do
-  let(:scope) { double }
-
   context 'custom sorting class exists' do
     subject { User }
+    let(:scope) { User.where(nil) }
     let(:sorting_class) { UserSorting }
 
     it 'should delegate sorting to sorting class' do
-      expected_to_sort_with 'name_asc', sorting_class
+      expected_to_sort_with 'name_asc', sorting_class, scope
     end
   end
 
   context 'custom sorting class doesnt exists' do
     subject { Group }
+    let(:scope) { Group.where(nil) }
     let(:sorting_class) { ActiveRecordSorting::Base }
 
     it 'should delegate sorting to base sorting class' do
-      expected_to_sort_with 'name_asc', sorting_class
+      expected_to_sort_with 'name_asc', sorting_class, scope
     end
   end
 
   context 'use scopes' do
     subject { Group.where(name: 'Lorem') }
+    let(:scope) { subject }
     let(:sorting_class) { ActiveRecordSorting::Base }
 
     it 'should delegate sorting to base sorting class' do
-      expected_to_sort_with 'name_asc', sorting_class
+      expected_to_sort_with 'name_asc', sorting_class, scope
     end
   end
 
   private
 
-  def expected_to_sort_with(order, sorting_class)
-    expect(sorting_class).to receive(:new).with(subject).and_call_original
+  def expected_to_sort_with(order, sorting_class, scope)
+    expect(sorting_class).to receive(:new).with(scope).and_call_original
     expect_any_instance_of(sorting_class).
       to receive(:sort).with(order).and_return scope
 
